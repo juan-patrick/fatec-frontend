6d6d7f19d6edfb2b7783dabe90d0f2a6db714e54
juan-patrick <juan-p.andrade@outlook.com>
2021-05-11 18:41:17 -0300

    feat: create home page

---
+++ src/pages/Home/index.js
@@ -0,0 +1,15 @@
+import Page from '../../components/Page';
+
+function Home() {
+  return (
+    <Page>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+      <h1>Home</h1>
+    </Page>
+  );
+}
+
+export default Home;
