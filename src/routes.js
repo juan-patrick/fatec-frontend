8703e5c1ba087fbd6a3373f9bc4b30a1eb5c1db3
juan-patrick <juan-p.andrade@outlook.com>
2021-06-11 19:29:58 -0300

    feat: add cnpj and cpf validator & format provider

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import { SnackbarProvider } from 'notistack';
 
+import ValidationProvider from './providers/ValidationProvider';
+
 import CursoExtensao from "./pages/CursoExtensao";
 import Home from "./pages/Home";
 import CreateCursoExtensao from "./pages/CreateCursoExtensao";
@@ -27,33 +29,35 @@ export default function Routes() {
         horizontal: 'center',
       }}
     >
-      <BrowserRouter>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/cursos-extensao" component={CursoExtensao} />
-          <Route path="/create/cursos-extensao" component={CreateCursoExtensao} />
-          <Route
-            path="/update/cursos-extensao/:cursoId"
-            component={UpdateCursoExtensao}
-          />
-          <Route path="/cursos" component={Curso} />
-          <Route path="/create/cursos" component={CreateCurso} />
-          <Route path="/update/cursos/:cursoId" component={UpdateCurso} />
+      <ValidationProvider>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/cursos-extensao" component={CursoExtensao} />
+            <Route path="/create/cursos-extensao" component={CreateCursoExtensao} />
+            <Route
+              path="/update/cursos-extensao/:cursoId"
+              component={UpdateCursoExtensao}
+            />
+            <Route path="/cursos" component={Curso} />
+            <Route path="/create/cursos" component={CreateCurso} />
+            <Route path="/update/cursos/:cursoId" component={UpdateCurso} />
 
-          <Route path="/projeto" component={Projeto} />
-          <Route path="/create/projeto" component={CreateProjeto} />
-          <Route path="/update/projeto/:projetoId" component={UpdateProjeto} />
+            <Route path="/projeto" component={Projeto} />
+            <Route path="/create/projeto" component={CreateProjeto} />
+            <Route path="/update/projeto/:projetoId" component={UpdateProjeto} />
 
-          <Route path="/horario" component={Horario} />
-          <Route path="/create/horario" component={CreateHorario} />
-          <Route path="/update/horario/:horarioId" component={UpdateHorario} />
+            <Route path="/horario" component={Horario} />
+            <Route path="/create/horario" component={CreateHorario} />
+            <Route path="/update/horario/:horarioId" component={UpdateHorario} />
 
-          <Route path="/turma" component={Turma} />
-          <Route path="/create/turma" component={CreateTurma} />
-          <Route path="/update/turma/:turmaId" component={UpdateTurma} />
+            <Route path="/turma" component={Turma} />
+            <Route path="/create/turma" component={CreateTurma} />
+            <Route path="/update/turma/:turmaId" component={UpdateTurma} />
 
-        </Switch>
-      </BrowserRouter>
+          </Switch>
+        </BrowserRouter>
+      </ValidationProvider>
     </SnackbarProvider>
   );
 }
