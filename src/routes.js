9e55082476978e1d34d10cbc2d512a99cb0b23d4
juan-patrick <juan-p.andrade@outlook.com>
2021-06-11 18:43:24 -0300

    feat: add notistack provider to add notifications

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,8 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import CursoExtensao from "./pages/CursoExtensao";
+import { SnackbarProvider } from 'notistack';
 
+import CursoExtensao from "./pages/CursoExtensao";
 import Home from "./pages/Home";
 import CreateCursoExtensao from "./pages/CreateCursoExtensao";
 import UpdateCursoExtensao from "./pages/UpdateCursoExtensao";
@@ -20,32 +21,39 @@ import UpdateTurma from "./pages/UpdateTurma";
 
 export default function Routes() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/cursos-extensao" component={CursoExtensao} />
-        <Route path="/create/cursos-extensao" component={CreateCursoExtensao} />
-        <Route
-          path="/update/cursos-extensao/:cursoId"
-          component={UpdateCursoExtensao}
-        />
-        <Route path="/cursos" component={Curso} />
-        <Route path="/create/cursos" component={CreateCurso} />
-        <Route path="/update/cursos/:cursoId" component={UpdateCurso} />
+    <SnackbarProvider
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'center',
+      }}
+    >
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/cursos-extensao" component={CursoExtensao} />
+          <Route path="/create/cursos-extensao" component={CreateCursoExtensao} />
+          <Route
+            path="/update/cursos-extensao/:cursoId"
+            component={UpdateCursoExtensao}
+          />
+          <Route path="/cursos" component={Curso} />
+          <Route path="/create/cursos" component={CreateCurso} />
+          <Route path="/update/cursos/:cursoId" component={UpdateCurso} />
 
-        <Route path="/projeto" component={Projeto} />
-        <Route path="/create/projeto" component={CreateProjeto} /> 
-        <Route path="/update/projeto/:projetoId" component={UpdateProjeto} />  
+          <Route path="/projeto" component={Projeto} />
+          <Route path="/create/projeto" component={CreateProjeto} />
+          <Route path="/update/projeto/:projetoId" component={UpdateProjeto} />
 
-        <Route path="/horario" component={Horario} />
-        <Route path="/create/horario" component={CreateHorario} /> 
-        <Route path="/update/horario/:horarioId" component={UpdateHorario} /> 
+          <Route path="/horario" component={Horario} />
+          <Route path="/create/horario" component={CreateHorario} />
+          <Route path="/update/horario/:horarioId" component={UpdateHorario} />
 
-        <Route path="/turma" component={Turma} />
-        <Route path="/create/turma" component={CreateTurma} /> 
-        <Route path="/update/turma/:turmaId" component={UpdateTurma} />   
+          <Route path="/turma" component={Turma} />
+          <Route path="/create/turma" component={CreateTurma} />
+          <Route path="/update/turma/:turmaId" component={UpdateTurma} />
 
-      </Switch>
-    </BrowserRouter>
+        </Switch>
+      </BrowserRouter>
+    </SnackbarProvider>
   );
 }
